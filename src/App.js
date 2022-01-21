05e0ff80595d8451ccf84eda2733a5155940fed0
zhengxingXue <zx252cornell@gmail.com>
2022-01-21 17:56:34 -0500

    material ui setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import * as React from 'react';
+import { Box, Grid, Typography } from '@mui/material';
+
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <React.Fragment>
+      <Box
+        sx={{
+          minHeight: '100vh',
+          backgroundImage: `url(${process.env.PUBLIC_URL + '/assets/bg.jpeg'})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        }}
+      />
+
+      <Grid
+        container
+        direction='column'
+        justifyContent='space-evenly'
+        alignItems='center'
+        spacing={0}
+        sx={{ minHeight: '100vh' }}
+      >
+        <Grid item>
+          <Typography variant='h1'>Hello</Typography>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom';
 
+// MUI import
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+
+// Project import
 import App from './App';
+import theme from './components/theme';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
   document.getElementById('root')
 );
