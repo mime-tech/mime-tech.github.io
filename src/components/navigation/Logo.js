66b808d1f2f33359ee5585cc5e1dc28f5ee00a5f
zhengxingXue <zx252cornell@gmail.com>
2022-01-26 12:29:54 -0500

    add link to logo

--- src/components/navigation/Logo.js
+++ src/components/navigation/Logo.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
-import { Typography } from '@mui/material';
+import { Typography, Link } from '@mui/material';
 
 const Logo = (props) => {
   const logoText = 'M';
 
   return (
-    <Typography
-      variant='h3'
-      noWrap
-      component='div'
-      sx={{ display: 'flex', fontFamily: 'Zen Tokyo Zoo', ...props }}
-    >
-      {logoText}
-    </Typography>
+    <Link color='inherit' underline='none' href='https://mime-tech.github.io/'>
+      <Typography
+        variant='h3'
+        noWrap
+        component='div'
+        sx={{ display: 'flex', fontFamily: 'Zen Tokyo Zoo', ...props }}
+      >
+        {logoText}
+      </Typography>
+    </Link>
   );
 };
 
